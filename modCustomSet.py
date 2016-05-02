3b7418f9c94d435dfef202f19148eb3ef6f89f9c
Karma Tsering Gurung <Karma@Karmas-MacBook-Pro.local>
2016-05-02 01:10:20 -0500

    anisha added fuction

--- modCustomSet.py
+++ modCustomSet.py
@@ -74,6 +74,39 @@ class CustomSet:
             return "yes"
         else:
             return "not there"
+
+    def subset(self,tmp1,tmp2):
+        set1 = CustomSet.__init__(self,tmp1)
+        set2 = CustomSet.__init__(self, tmp2)
+        ct = 0
+        for el in tmp1:
+            if el in tmp2:
+                ct +=1
+        if ct == len(tmp1):
+            return ("Yes")
+        if ct < len(tmp1):
+            return ("not a subset")
+
+    
+    def superSet(self,tmp1,tmp2):
+        set1 = CustomSet.__init__(self,tmp1)
+        set2 = CustomSet.__init__(self, tmp2)
+        ct = 0
+        for el in tmp1:
+            if el in tmp2:
+                ct +=1
+        if ct == len(tmp1) and len(tmp1) < len(tmp2):
+            return ("Yes")
+        else:
+            return ("No")
+
+    def count(self, tmp1):
+        set1 = CustomSet.__init__(self,tmp1)
+        ct = 0
+        for el in tmp1:
+            ct += 1
+        return ct
+    
     def __str__(self):
         """
         __author__="Karma Gurung"
